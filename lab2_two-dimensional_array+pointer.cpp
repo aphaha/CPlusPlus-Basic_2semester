e76955db11e34f9c4ddadd25d2219e8bd50a2d31
Maria <masha200430mm@gmail.com>
2023-04-03 13:13:56 +0300

    Lab2

---
+++ lab2_two-dimensional_array+pointer.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int matrix[10][10] = { };
+    
+    for (int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            *(*(matrix + i) + j) = rand() % 10;
+            
+        }
+    }
+    
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            cout.width(4);
+            cout << matrix[i][j];
+        }
+        cout << endl;
+    }
+    
+    cout << endl;
+    cout << "   The main diagonal: ";
+    
+    for (int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            if (i == j) cout << *(*(matrix + i) + j) << " ";
+            
+        }
+    } 
+     
+    cout << endl;
+    cout << "   The side diagonal: "; 
+    
+    for (int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            if (i + j == 9) cout << *(*(matrix + i) + j) << " ";
+            
+        }
+    }
+    
+    int n = 0;
+    
+    for (int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            if (*(*(matrix + i) + j) % 2 == 0){ //&& *(*(matrix + i) + j) != 0
+                n++;
+            }
+        }
+    }
+    
+    cout << endl;
+    cout << "   Total of even numbers: " << n; 
+    
+    int o = 0;
+    
+    for (int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            if (*(*(matrix + i) + j) % 2 != 0){ 
+                o++;
+            }
+        }
+    }
+    
+    cout << endl;
+    cout << "   Total of odd numbers: " << o; 
+    
+    return 0;
+}
